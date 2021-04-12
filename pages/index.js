89d824e232fd4a6e76b08af194f7dc68da404333
alex1500ism1 <69977496+alex1500ism1@users.noreply.github.com>
2021-04-12 15:24:34 -0500

    tu negocio fornt

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Layout from '../components/lauout'
+
+const Index = () => (
+  <div>
+    <Layout>
+      <h2>Desde Index</h2>
+    </Layout>
+  </div>
+)
+
+export default Index;
